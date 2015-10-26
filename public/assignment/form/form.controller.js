d0848070e809a7d54ff3033d064533ad071397bd
yixing <yixing@ccs.neu.edu>
2015-10-25 23:28:36 -0400

    finished profile, form

--- public/assignment/form/form.controller.js
+++ public/assignment/form/form.controller.js
@@ -1,3 +1,53 @@
 /**
  * Created by yixing on 10/25/15.
  */
+
+"use strict";
+
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .controller("FormController", FormController);
+
+    function FormController($rootScope, $scope, $location, FormService) {
+        $scope.$location = $location;
+        $scope.newForm = {};
+        $scope.addForm = addForm;
+        $scope.updateForm = updateForm;
+        $scope.selectForm = selectForm;
+        $scope.deleteForm = deleteForm;
+
+        $scope.forms = [];
+
+        var userid = $rootScope.currentUser.id;
+
+        FormService.findAllFormsForUser(userid, function(userForms){
+            $scope.forms = userForms;
+        });
+
+        function addForm() {
+            FormService.createFormForUser(userid, angular.extend({}, $scope.newForm), function(form) {
+                $scope.forms.push(form);
+            });
+        }
+
+        function selectForm(index) {
+            $scope.selectedForm = $scope.forms[index];
+            $scope.newForm.name = $scope.selectedForm.name;
+        }
+
+        function updateForm() {
+            if ($scope.selectedForm) {
+                FormService.updateFormById($scope.selectedForm.id, $scope.newForm, function(updatedForm) {
+                });
+            }
+        }
+
+        function deleteForm(index) {
+            FormService.deleteFormById($scope.forms[index].id, function(forms){
+                $scope.forms.splice(index, 1);
+            });
+        }
+
+    }
+})();
--- public/assignment/profile/profile.controller.js
+++ public/assignment/profile/profile.controller.js
@@ -1,3 +1,32 @@
 /**
  * Created by yixing on 10/25/15.
  */
+"use strict";
+
+(function() {
+    angular
+        .module("BuilderFormApp")
+        .controller("ProfileController", ProfileController)
+
+    function ProfileController($rootScope, $scope, $location, UserService) {
+        $scope.$location = $location;
+        $scope.update = update;
+        var user = $rootScope.ser;
+
+        function update() {
+            var newUser = {
+                username: $scope.username,
+                password: $scope.password,
+                firstname: $scope.fisrtname,
+                lastname: $scope.lastname,
+                email: $scope.email
+            };
+            UserService.updateUser(user.userid, newUser, function(user) {
+                $scope.user = user;
+                $rootScope.user = user;
+            });
+            $location.url('/profile');
+        }
+
+    }
+})();
